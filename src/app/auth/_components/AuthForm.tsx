bbd8a57034de27f6a630d2b048a78cce3ca89067
Hosein Alaei <alaei.h@pdn.co>
2024-12-30 12:04:19 +0330

    change httponly21

--- src/app/auth/_components/AuthForm.tsx
+++ src/app/auth/_components/AuthForm.tsx
@@ -7,86 +7,83 @@ import { useRouter } from "next/navigation";
 // import { useState } from "react";
 // import { signIn } from "next-auth/react";
 
-const AuthForm = ({ handleSubmit }: any) =>
-  // getCaptcha,
-  // captcha,
-  // isLoading,
-  // setUsername,
-  // setPassword,
-  // insertCaptcha,
-  // setInsertCaptcha,
-  {
-    // const router = useRouter();
-    // async function handleSubmit(formData: FormData) {
-    //   // const body = {
-    //   //   username: formData.get("username") as string,
-    //   //   password: formData.get("password") as string,
-    //   //   captcha_text: formData.get("captcha_text") as string,
-    //   //   captcha_id: formData.get("captcha_id") as string,
-    //   //   callbackUrl: "/dashboard/home",
-    //   // };
-    //   //"credentials",
-    //   const result = await signIn({
-    //     username: formData.get("username") as string,
-    //     password: formData.get("password") as string,
-    //     captcha_text: formData.get("captcha_text") as string,
-    //     captcha_id: formData.get("captcha_id") as string,
-    //     // callbackUrl: "/dashboard/home",
-    //   });
-    //   console.log("result", result);
-    //   if (result?.data?.jwt) {
-    //     router.push("/dashboard/home");
-    //   }
-    // }
+const AuthForm = ({
+  handleSubmit,
+  getCaptcha,
+  captcha,
+  isLoading,
+  setUsername,
+  setPassword,
+  insertCaptcha,
+  setInsertCaptcha,
+}: any) => {
+  // const router = useRouter();
+  // async function handleSubmit(formData: FormData) {
+  //   // const body = {
+  //   //   username: formData.get("username") as string,
+  //   //   password: formData.get("password") as string,
+  //   //   captcha_text: formData.get("captcha_text") as string,
+  //   //   captcha_id: formData.get("captcha_id") as string,
+  //   //   callbackUrl: "/dashboard/home",
+  //   // };
+  //   //"credentials",
+  //   const result = await signIn({
+  //     username: formData.get("username") as string,
+  //     password: formData.get("password") as string,
+  //     captcha_text: formData.get("captcha_text") as string,
+  //     captcha_id: formData.get("captcha_id") as string,
+  //     // callbackUrl: "/dashboard/home",
+  //   });
+  //   console.log("result", result);
+  //   if (result?.data?.jwt) {
+  //     router.push("/dashboard/home");
+  //   }
+  // }
 
-    return (
-      <form
-        className="w-full"
-        onSubmit={handleSubmit}
-        // action={(e) => {
-        //   // e.preventDefault();
-        //   // const formData = new FormData(e.currentTarget);
-        //   handleSubmit();
-        // }}
-      >
-        <div className="flex gap-6 my-4 justify-between items-center">
-          <label>شناسه کاربری:</label>
-          <Input
-            name="username"
-            // onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="flex gap-6 my-4 justify-between items-center">
-          <label>رمز عبور:</label>
-          <Input
-            type="password"
-            name="password"
-            // onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <label>کد امنیتی:</label>
+  return (
+    <form
+      className="w-full"
+      onSubmit={handleSubmit}
+      // action={(e) => {
+      //   // e.preventDefault();
+      //   // const formData = new FormData(e.currentTarget);
+      //   handleSubmit();
+      // }}
+    >
+      <div className="flex gap-6 my-4 justify-between items-center">
+        <label>شناسه کاربری:</label>
+        <Input name="username" onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div className="flex gap-6 my-4 justify-between items-center">
+        <label>رمز عبور:</label>
+        <Input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex justify-between items-center">
+        <label>کد امنیتی:</label>
 
-          <Captcha
-
-          // getCaptcha={getCaptcha}
-          // captcha={captcha}
-          // setInsertCaptcha={setInsertCaptcha}
-          />
-        </div>
-        <div className="w-full my-6">
-          <Button
-            variant="primary"
-            type="submit"
-            className="w-full"
-            // isLoading={isLoading}
-            // isDisabled={insertCaptcha?.length < 5 ? true : false}
-          >
-            ورود
-          </Button>
-        </div>
-      </form>
-    );
-  };
+        <Captcha
+          getCaptcha={getCaptcha}
+          captcha={captcha}
+          setInsertCaptcha={setInsertCaptcha}
+        />
+      </div>
+      <div className="w-full my-6">
+        <Button
+          variant="primary"
+          type="submit"
+          className="w-full"
+          // isLoading={isLoading}
+          // isDisabled={insertCaptcha?.length < 5 ? true : false}
+        >
+          ورود
+        </Button>
+      </div>
+    </form>
+  );
+};
 
 export default AuthForm;
