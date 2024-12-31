d02a75b66f6ddc939dfbcac60aac209dfdbeb03f
Hosein Alaei <alaei.h@pdn.co>
2024-12-31 11:08:40 +0330

    change httponly33

--- src/app/auth/_components/AuthForm.tsx
+++ src/app/auth/_components/AuthForm.tsx
@@ -12,105 +12,78 @@ import { useRouter } from "next/navigation";
 // import { useState } from "react";
 // import { signIn } from "next-auth/react";
 
-const AuthForm = () =>
-  // getCaptcha,
-  // captcha,
-  // isLoading,
-  // setUsername,
-  // setPassword,
-  // insertCaptcha,
-  // setInsertCaptcha,
-  {
-    const router = useRouter();
-    async function handleSubmit(formData: FormData) {
-      // const body = {
-      //   username: formData.get("username") as string,
-      //   password: formData.get("password") as string,
-      //   captcha_text: formData.get("captcha_text") as string,
-      //   captcha_id: formData.get("captcha_id") as string,
-      //   callbackUrl: "/dashboard/home",
-      // };
-      //"credentials",
-      const result = await signIn({
+const AuthForm = () => {
+  const router = useRouter();
+
+  const clientSideSubmit = async (formData: FormData) => {
+    try {
+      const response = await axios.post(`${API_URL}${Apies.Login}`, {
         username: formData.get("username") as string,
         password: formData.get("password") as string,
         captcha_text: formData.get("captcha_text") as string,
         captcha_id: formData.get("captcha_id") as string,
-        // callbackUrl: "/dashboard/home",
       });
-      console.log("result", result);
-      if (result?.data?.jwt) {
+      console.log("response", response);
+      if (response.data.data.jwt) {
+        setCookie(cookieName, response.data.data.jwt, {
+          httpOnly: false,
+          secure: process.env.NODE_ENV === "production",
+        });
         router.push("/dashboard/home");
       }
+    } catch (e: any) {
+      console.log(e);
     }
-
-    const clientSideSubmit = async (formData: FormData) => {
-      // e.preventDefault()
-      try {
-        const response = await axios.post(`${API_URL}${Apies.Login}`, {
-          username: formData.get("username") as string,
-          password: formData.get("password") as string,
-          captcha_text: formData.get("captcha_text") as string,
-          captcha_id: formData.get("captcha_id") as string,
-        });
-        console.log("response", response);
-        if (response.data.data.jwt) {
-          setCookie(cookieName, response.data.data.jwt);
-          router.push("/dashboard/home");
-        }
-      } catch (e: any) {
-        console.log(e);
-      }
-    };
-
-    return (
-      <form
-        className="w-full"
-        action={clientSideSubmit}
-        // action={(e) => {
-        //   // e.preventDefault();
-        //   // const formData = new FormData(e.currentTarget);
-        //   handleSubmit();
-        // }}
-      >
-        <div className="flex gap-6 my-4 justify-between items-center">
-          <label>شناسه کاربری:</label>
-          <Input
-            name="username"
-            // onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="flex gap-6 my-4 justify-between items-center">
-          <label>رمز عبور:</label>
-          <Input
-            type="password"
-            name="password"
-            // onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <label>کد امنیتی:</label>
-
-          <Captcha
-
-          // getCaptcha={getCaptcha}
-          // captcha={captcha}
-          // setInsertCaptcha={setInsertCaptcha}
-          />
-        </div>
-        <div className="w-full my-6">
-          <Button
-            variant="primary"
-            type="submit"
-            className="w-full"
-            // isLoading={isLoading}
-            // isDisabled={insertCaptcha?.length < 5 ? true : false}
-          >
-            ورود
-          </Button>
-        </div>
-      </form>
-    );
   };
+
+  return (
+    <form
+      className="w-full"
+      action={clientSideSubmit}
+      // action={(e) => {
+      //   // e.preventDefault();
+      //   // const formData = new FormData(e.currentTarget);
+      //   handleSubmit();
+      // }}
+    >
+      <div className="flex gap-6 my-4 justify-between items-center">
+        <label>شناسه کاربری:</label>
+        <Input
+          name="username"
+          // onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="flex gap-6 my-4 justify-between items-center">
+        <label>رمز عبور:</label>
+        <Input
+          type="password"
+          name="password"
+          // onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex justify-between items-center">
+        <label>کد امنیتی:</label>
+
+        <Captcha
+
+        // getCaptcha={getCaptcha}
+        // captcha={captcha}
+        // setInsertCaptcha={setInsertCaptcha}
+        />
+      </div>
+      <div className="w-full my-6">
+        <Button
+          variant="primary"
+          type="submit"
+          className="w-full"
+          // isLoading={isLoading}
+          // isDisabled={insertCaptcha?.length < 5 ? true : false}
+        >
+          ورود
+        </Button>
+      </div>
+    </form>
+  );
+};
 
 export default AuthForm;
