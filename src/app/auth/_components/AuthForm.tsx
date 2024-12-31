61eca51fc0b0af7a0cf042620c977d247ae11913
Hosein Alaei <alaei.h@pdn.co>
2024-12-31 10:55:12 +0330

    change httponly31

--- src/app/auth/_components/AuthForm.tsx
+++ src/app/auth/_components/AuthForm.tsx
@@ -2,7 +2,10 @@
 import Button from "@/app/_component/Button";
 import Captcha from "@/app/_component/Captcha/Captcha";
 import Input from "@/app/_component/Form/Input";
+import { API_URL } from "@/configs/global";
+import { Apies } from "@/constant/apis";
 import { signIn } from "@/lib/auth/signIn";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 // import { useState } from "react";
 // import { signIn } from "next-auth/react";
@@ -39,10 +42,25 @@ const AuthForm = () =>
       }
     }
 
+    const clientSideSubmit = async (formData: FormData) => {
+      // e.preventDefault()
+      try {
+        const response = await axios.post(`${API_URL}${Apies.Login}`, {
+          username: formData.get("username") as string,
+          password: formData.get("password") as string,
+          captcha_text: formData.get("captcha_text") as string,
+          captcha_id: formData.get("captcha_id") as string,
+        });
+        console.log("response", response);
+      } catch (e: any) {
+        console.log(e);
+      }
+    };
+
     return (
       <form
         className="w-full"
-        action={handleSubmit}
+        action={clientSideSubmit}
         // action={(e) => {
         //   // e.preventDefault();
         //   // const formData = new FormData(e.currentTarget);
