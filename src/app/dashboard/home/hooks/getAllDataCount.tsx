3201023a9a27e58bd8622c31e60cd47eab741f29
Hosein Alaei <alaei.h@pdn.co>
2024-12-31 11:31:53 +0330

    change httponly37

--- src/app/dashboard/home/hooks/getAllDataCount.tsx
+++ src/app/dashboard/home/hooks/getAllDataCount.tsx
@@ -1,22 +1,9 @@
-import { API_URL } from "@/configs/global";
 import { Apies } from "@/constant/apis";
 import { getData } from "@/core/http-service/http-service";
-import { cookieName } from "@/lib/auth/constant";
-import axios from "axios";
-import { getCookie } from "cookies-next";
 import { useQuery } from "react-query";
 
 const getAllDataCount = async (): Promise<DataType> => {
-  const token = getCookie(cookieName);
-  const response = await axios.get(`${API_URL}${Apies.GetAllDataCount}`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  console.log("eeeeeeeeee", response);
-
-  return response.data;
+  return getData(Apies.GetAllDataCount);
 };
 
 export const useGetAllDataCount = () => {
